1ca18cdeec1a4fb9bc46c0b9a9d85994550e26af
SCIENARTBB <scienart@SCIENARTBBs-MacBook-Pro.local>
2017-05-24 13:24:27 +0900

    update

---
+++ SBMatrixTest/SBMatrixTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SBMatrixTest
+//
+//  Created by SCIENARTBB on 2017. 5. 24..
+//  Copyright © 2017년 CoCO SWING. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SBMatrixTest/SBMatrixTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SBMatrixTest
+//
+//  Created by SCIENARTBB on 2017. 5. 24..
+//  Copyright © 2017년 CoCO SWING. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
